dbb369eb57dc0350ebd390eea09cc5c98e89120d
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-21 10:09:22 -0700

    movies hooked up to supabase and displaying on the Movies page

---
+++ src/pages/Movies.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import { fetchMovies } from '../services/movies';
+
+function Movies() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetchMovies();
+      console.log(resp);
+      setMovies(resp);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <p key={movie.id}>{movie.title}</p>
+      ))}
+    </div>
+  );
+}
+
+export default Movies;
