8e66ea03250b45f68475f1debd27acefb41ccc3c
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-21 19:39:12 -0700

    bulletin post titles updating in view

--- src/pages/BulletinBoard.js
+++ src/pages/BulletinBoard.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { fetchBoard } from '../services/bulletinboard';
+import { useParams } from 'react-router-dom';
 
 function BulletinBoard() {
-  return <div>BulletinBoard</div>;
+  const params = useParams();
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchBoard();
+      setPosts(data);
+    };
+    fetchData();
+  }, [params.name]);
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <p key={post.id}>{post.title}</p>
+      ))}
+    </div>
+  );
 }
 
 export default BulletinBoard;
