57ea6d8f094f0133416388ad77e6314aec2883a4
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-22 14:02:38 -0700

    refactored movies fetch

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,6 +1,19 @@
 import { client, checkError } from './client';
 
 export async function fetchMovies() {
-  const resp = await client.from('movies').select();
-  return checkError(resp);
+  //   const resp = await client.from('movies').select();
+  //   return checkError(resp);
+  const params = new URLSearchParams();
+  params.set('select', '*');
+  const resp = await fetch(
+    `${process.env.REACT_APP_SUPABASE_URL}/rest/v1/movies?${params.toString()}`,
+    {
+      headers: {
+        apikey: process.env.REACT_APP_SUPABASE_KEY,
+        Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
+      },
+    }
+  );
+  const data = await resp.json();
+  return data;
 }
