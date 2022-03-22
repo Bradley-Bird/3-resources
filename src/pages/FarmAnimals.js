f6b84c5a87a3cef1bfd7a9bdeb27d0a19b7d0e26
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-21 19:47:45 -0700

    farm animals hooked up and displaying to page

--- src/pages/FarmAnimals.js
+++ src/pages/FarmAnimals.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { fetchAnimals } from '../services/farmanimals';
+import { useParams } from 'react-router-dom';
 
 function FarmAnimals() {
-  return <div>FarmAnimals</div>;
+  const params = useParams();
+  const [animals, setAnimals] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchAnimals();
+      setAnimals(data);
+    };
+    fetchData();
+  }, [params.name]);
+
+  return (
+    <div>
+      {animals.map((animal) => (
+        <p key={animal.id}>{animal.name}</p>
+      ))}
+    </div>
+  );
 }
 
 export default FarmAnimals;
